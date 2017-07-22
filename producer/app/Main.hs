dc98a0f366c2b907c09dc81bf5c0bd86324831df
Philip Cunningham <hello@filib.io>
2017-07-22 15:30:30 +0100

    add relatively straightforward client and add debugging to server/client.

--- producer/app/Main.hs
+++ producer/app/Main.hs
@@ -2,15 +2,30 @@
 
 module Main where
 
-import           Data.Maybe (fromMaybe)
+import           Control.Concurrent (forkIO)
+import           Control.Monad (forever)
+import           Control.Monad.Trans (liftIO)
+import           Data.Maybe (fromMaybe, listToMaybe)
+import           Data.Monoid ((<>))
 import           Data.Text (Text)
+import qualified Data.Text.IO as T
+import           Network.Socket (withSocketsDo)
 import qualified Network.WebSockets as WS
-import           System.Environment (lookupEnv)
+import           System.Environment (getArgs, lookupEnv)
 
 main :: IO ()
-main = server
+main = do
+  args <- getArgs
+  case listToMaybe args of
+    Just "client" ->
+      client
+    _             ->
+      server
 
 -- CONFIG
+
+ack :: Text
+ack = "ACK"
 
 defaultAddress :: String
 defaultAddress = "127.0.0.1"
@@ -39,14 +54,49 @@ server :: IO ()
 server = do
   address <- getAddress
   port    <- getPort
-  WS.runServer address port handleWebSocket
+  serverStarting address port
+  WS.runServer address port serverConnection
 
 -- SERVER : WEBSOCKETS
 
-ack :: Text
-ack = "ACK"
-
-handleWebSocket :: WS.PendingConnection -> IO ()
-handleWebSocket pending = do
+serverConnection :: WS.PendingConnection -> IO ()
+serverConnection pending = do
+  serverConnected
   connection <- WS.acceptRequest pending
+  serverAccepted
   WS.sendTextData connection ack
+
+-- SERVER : LOGGING
+
+serverStarting :: String -> Int -> IO ()
+serverStarting address port = putStrLn $
+  "Server starting on: " <> address <> ":" <> (show port)
+
+serverAccepted :: IO ()
+serverAccepted = putStrLn "Server accepted"
+
+serverConnected :: IO ()
+serverConnected = putStrLn "Client connected"
+
+-- CLIENT
+
+client :: IO ()
+client = withSocketsDo $ do
+  address <- getAddress
+  port    <- getPort
+  WS.runClient address port "/" clientConnection
+
+-- CLIENT : WEBSOCKETS
+
+clientConnection :: WS.ClientApp ()
+clientConnection connection = do
+  clientConnected
+  _ <- forkIO $ forever $ do
+    message <- WS.receiveData connection
+    liftIO $ T.putStrLn message
+  return ()
+
+-- CLIENT : LOGGING
+
+clientConnected :: IO ()
+clientConnected = putStrLn "Connected"
