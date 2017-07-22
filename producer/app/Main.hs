15c3e6d1123ab5de1a6b920cc7c211fa8691d655
Philip Cunningham <hello@filib.io>
2017-07-22 15:00:21 +0100

    add initial stub of the server.

---
+++ producer/app/Main.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import           Data.Maybe (fromMaybe)
+import qualified Network.WebSockets as WS
+import           System.Environment (lookupEnv)
+
+main :: IO ()
+main = do
+  address <- getAddress
+  port    <- getPort
+  WS.runServer address port handleWebSocket
+
+-- CONFIG
+
+defaultAddress :: String
+defaultAddress = "127.0.0.1"
+
+defaultPort :: Int
+defaultPort = 5000
+
+getAddress :: IO String
+getAddress =
+  getConfig "PRODUCER_ADDRESS" defaultAddress
+
+getPort :: IO Int
+getPort =
+  getConfig "PRODUCER_PORT" defaultPort
+
+getConfig :: Read a => String -> a -> IO a
+getConfig key fallback = do
+  maybeEnv <- lookupEnv key
+  return $ case maybeEnv of
+    Just string -> read string
+    Nothing     -> fallback
+
+-- WEBSOCKETS
+
+handleWebSocket :: WS.PendingConnection -> IO ()
+handleWebSocket pending = do
+  connection <- WS.acceptRequest pending
+  WS.sendTextData connection ("Ciao" :: Text)
