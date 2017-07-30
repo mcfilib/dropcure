7352850bbd7143b115c84daef98669a4d1161aae
Philip Cunningham <hello@filib.io>
2017-07-30 13:24:24 +0100

    simplify server.

--- consumer/app/Main.hs
+++ consumer/app/Main.hs
@@ -6,23 +6,21 @@ import           Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, rea
 import           Control.Exception (finally)
 import           Control.Monad (forM_, forever)
 import           Data.Char (isPunctuation, isSpace)
+import qualified Data.Map.Strict as DM
 import           Data.Monoid (mempty, mappend)
 import           Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import           Data.Unique (Unique, newUnique)
 import qualified Network.WebSockets as WS
 
-type Client = (Text, WS.Connection)
+type Client = (Unique, WS.Connection)
 
 type ServerState = [Client]
 
 newServerState :: ServerState
 newServerState =
   mempty
-
-clientExists :: Client -> ServerState -> Bool
-clientExists client =
-  any ((== fst client) . fst)
 
 addClient :: Client -> ServerState -> ServerState
 addClient client clients =
@@ -37,46 +35,35 @@ broadcast message clients = do
     forM_ clients $ \(_, conn) ->
       WS.sendTextData conn message
 
-application state pending = do
-    conn <- WS.acceptRequest pending
-    WS.forkPingThread conn 30
-    msg <- WS.receiveData conn
-    clients <- readMVar state
-    case msg of
-        _   | not (prefix `T.isPrefixOf` msg) ->
-                WS.sendTextData conn ("Wrong announcement" :: Text)
-            | any ($ fst client)
-                [T.null, T.any isPunctuation, T.any isSpace] ->
-                    WS.sendTextData conn ("Name cannot " `mappend`
-                        "contain punctuation or whitespace, and " `mappend`
-                        "cannot be empty" :: Text)
-            | clientExists client clients ->
-                WS.sendTextData conn ("User already exists" :: Text)
-            | otherwise -> flip finally disconnect $ do
-               modifyMVar_ state $ \s -> do
-                   let s' = addClient client s
-                   WS.sendTextData conn $
-                       "Welcome! Users: " `mappend`
-                       T.intercalate ", " (map fst s)
-                   broadcast (fst client `mappend` " joined") s'
-                   return s'
-               talk conn state client
-          where
-            prefix     = "Hi! I am "
-            client     = (T.drop (T.length prefix) msg, conn)
-            disconnect = do
-                -- Remove client and return new state
-                s <- modifyMVar state $ \s ->
-                    let s' = removeClient client s in return (s', s')
-                broadcast (fst client `mappend` " disconnected") s
+app :: MVar ServerState -> WS.ServerApp
+app state pending = do
+    connection <- WS.acceptRequest pending
+    _          <- addKeepAlive connection
+    newID      <- newUnique
+    finally (addListener newID connection) (removeListenever newID connection)
+    where
+      addKeepAlive :: WS.Connection -> IO ()
+      addKeepAlive connection =
+        WS.forkPingThread connection 30
 
-talk :: WS.Connection -> MVar ServerState -> Client -> IO ()
-talk conn state (user, _) = forever $ do
-    msg <- WS.receiveData conn
-    readMVar state >>= broadcast
-        (user `mappend` ": " `mappend` msg)
+      addListener :: Unique -> WS.Connection -> IO ()
+      addListener newID connection = do
+        modifyMVar_ state $ \oldState ->
+          return (addClient (newID, connection) oldState)
+        listen connection state (newID, connection)
+
+      removeListenever :: Unique -> WS.Connection -> IO ()
+      removeListenever newID connection = do
+        modifyMVar_ state $ \oldState -> do
+          return (removeClient (newID, connection) oldState)
+
+
+
+listen :: WS.Connection -> MVar ServerState -> Client -> IO ()
+listen conn state (user, _) = forever $ do
+    readMVar state >>= broadcast ("hello" :: Text)
 
 main :: IO ()
 main = do
     state <- newMVar newServerState
-    WS.runServer "127.0.0.1" 9160 (application state)
+    WS.runServer "127.0.0.1" 9160 (app state)
